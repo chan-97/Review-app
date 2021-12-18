1c1d11c9ddc8cd3459952492752c096018f04c19
HwiChanYu <dbgnlcks34@gmail.com>
2021-12-18 21:59:47 +0900

    apply fonts

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import Home from "./screens/Home";
+import AppLoading from "expo-app-loading";
+import { useFonts, Inconsolata_900Black } from "@expo-google-fonts/inconsolata";
+import { Nunito_400Regular_Italic } from "@expo-google-fonts/nunito";
 
 export default function App() {
-  return (
-    <>
-      <Home />
-      <StatusBar style="auto" />
-    </>
-  );
+  const [fontsLoaded] = useFonts({ Inconsolata_900Black, Nunito_400Regular_Italic });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Home />
+        <StatusBar style="auto" />
+      </>
+    )
+  }
 }
--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,8 @@ import { StyleSheet, View, Text } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.headText}>Home</Text>
+      <Text style={styles.bodyText}>Lorem Ipsum ...</Text>
     </View>
   )
 }
@@ -12,5 +13,11 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     padding: 24
+  },
+  headText: {
+    fontFamily: "Inconsolata_900Black"
+  },
+  bodyText: {
+    fontFamily: "Nunito_400Regular_Italic"
   }
 })
